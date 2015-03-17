f1ca9cc0f59d56682412bf587875758d18f3d96b
c9s <yoanlin93@gmail.com>
2015-03-18 00:08:55 +0900

    Add fixture

--- tests/ClassTemplate/BracketedBlockTest.php
+++ tests/ClassTemplate/BracketedBlockTest.php
@@ -12,5 +12,19 @@ class BracketedBlockTest extends PHPUnit_Framework_TestCase
         $block[] = 'return $a + $b;';
         $this->assertStringEqualsFile('tests/data/bracketed_block_simple.fixture',$block->render());
     }
+
+    public function testSimpleBracketedBlockAndSubBlock()
+    {
+        $block = new BracketedBlock;
+        $block[] = '$a = 1;';
+        $block[] = '$b = 2;';
+
+        $subblock = new BracketedBlock;
+        $subblock[] = '$e = $a + $b;';
+        $subblock[] = '$f = $a * $b;';
+        $block[] = $subblock;
+        $block[] = 'return $a + $b;';
+        $this->assertStringEqualsFile('tests/data/bracketed_block_subblock.fixture', $block->render());
+    }
 }
 
