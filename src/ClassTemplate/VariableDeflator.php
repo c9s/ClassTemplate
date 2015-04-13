4e6c53829dcdf1163b90a007171e5a6a135e0d72
c9s <yoanlin93@gmail.com>
2015-04-13 18:39:39 +0800

    Extract the variable deflating code to VariableDeflator

---
+++ src/ClassTemplate/VariableDeflator.php
@@ -0,0 +1,29 @@
+<?php
+namespace ClassTemplate;
+use ClassTemplate\Raw;
+use ClassTemplate\Exportable;
+
+class VariableDeflator
+{
+    static public function deflate($var) 
+    {
+        // Raw string output
+        if (is_string($arg) && $arg[0] == '$') {
+            return $arg;
+        } else if ($arg instanceof Renderable) {
+            return $arg->render($args);
+        } else if ($arg instanceof Raw) {
+            return $arg;
+        } else if ($arg instanceof Exportable) {
+            return var_export($arg->__get_state(), true);
+        } else if (is_array($arg) || method_exists($arg,"__set_state") || is_scalar($arg)) {
+            return var_export($arg, true);
+        } else {
+            throw new LogicException("Can't deflate variable");
+        }
+    }
+}
+
+
+
+
